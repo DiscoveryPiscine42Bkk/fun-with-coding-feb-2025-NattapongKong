1d0053b13ca2da1f6239e79fbcf25bd807473bff
N.Kongwattananukulkit <47635925+NattapongKong@users.noreply.github.com>
2025-02-05 23:02:42 +0700

    10

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    for i in 1 2 3; do
+        if [ -n "${!i}" ]; then
+            echo "${!i}"
+        fi
+    done
+fi

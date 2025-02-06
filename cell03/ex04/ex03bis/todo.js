7d2a8bd7adca6f8455a7737d116417ede99b227e
N.Kongwattananukulkit <47635925+NattapongKong@users.noreply.github.com>
2025-02-06 21:03:20 +0700

    commit

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,9 +1,31 @@
+
 $(document).ready(function() {
     const ftList = $('#ft_list');
     const newTaskButton = $('#newTaskButton');
 
+    function setCookie(name, value, days) {
+        const expires = new Date();
+        expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+        document.cookie = name + "=" + value + ((expires==null) ? "" : ";expires=" + expires.toGMTString())
+    }
+
+    function getCookie(name) {
+        const cookies = document.cookie.split('; ');
+        for (let cookie of cookies) {
+            const [cookieName, cookieValue] = cookie.split('=');
+            if (cookieName === name) {
+                return decodeURIComponent(cookieValue);
+            }
+        }
+        return "";
+    }
+
+    function saveTasks() {
+        setCookie('tasks', ftList.html(), 7);
+    }
+
     function loadTasks() {
-        const tasks = localStorage.getItem('tasks');
+        const tasks = getCookie('tasks');
         if (tasks) {
             ftList.html(tasks);
             ftList.children().each(function() {
@@ -17,24 +39,20 @@ $(document).ready(function() {
         }
     }
 
-    function saveTasks() {
-        localStorage.setItem('tasks', ftList.html());
-    }
+    loadTasks();
 
     newTaskButton.on('click', function() {
-        const task = prompt("Enter a new task:");
-        if (task) {
-            const taskDiv = $('<div></div>').text(task);
-            taskDiv.on('click', function() {
+        const taskText = prompt("Enter a new task:");
+        if (taskText) {
+            const newTask = $('<div></div>').text(taskText);
+            newTask.on('click', function() {
                 if (confirm("Do you want to delete this task?")) {
                     $(this).remove();
                     saveTasks();
                 }
             });
-            ftList.prepend(taskDiv);
+            ftList.prepend(newTask);
             saveTasks();
         }
     });
-
-    loadTasks();
 });
